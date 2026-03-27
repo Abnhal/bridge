3fcdf94440aff7762d375e3e7bbe27e2f8a285dc
Abnhal <90050asdfghjkl@gmail.com>
2026-03-28 00:51:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const OFFLINE_TIMEOUT = 10000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+//app.get('/', (req, res) => {
+ // res.sendFile(path.join(__dirname, 'index.html'));
+//});
 
 // مجلد data للاحتياط (اختياري)
 if (!fs.existsSync('data')) {
