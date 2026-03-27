dab8e59fc73e247542e04ff194c762f12e1b47e6
Abnhal <90050asdfghjkl@gmail.com>
2026-03-28 00:42:23 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const THRESHOLD_CRITICAL = 2.0;
 const OFFLINE_TIMEOUT = 10000;
 
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
