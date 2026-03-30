a085fe27c4f44f802be8dfa9cfa7a26c324aeaac
Abnhal <90050asdfghjkl@gmail.com>
2026-03-30 23:51:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
 });
 
 // مجلد data للاحتياط (اختياري)
